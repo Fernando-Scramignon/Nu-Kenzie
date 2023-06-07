ea194a5d314f841b8cb403bf10efb7b86dd6aa0a
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-06 23:12:26 -0300

    feat: creates mobile version of welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,9 +1,17 @@
-import { Container } from './style'
+import { Container, ContentSection } from './style'
+
+import Button from '../../components/Button'
+import logoAlt from '../../assets/nu-kenzie-alt.svg'
 
 function Welcome() {
     return (
         <Container>
-            <h1>Welcome page goes here!!!</h1>
+            <ContentSection>
+                <img src={logoAlt} alt="Logo with alternative colors" />
+                <h1>Centralize o Controle das suas finanças</h1>
+                <p>De forma rápida e segura</p>
+                <Button>Iniciar</Button>
+            </ContentSection>
         </Container>
     )
 }
--- src/pages/Welcome/style.ts
+++ src/pages/Welcome/style.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+    min-height: 100vh;
+    padding: 0px 30px;
+    background-color: var(--grey-4);
 
-export { Container }
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const ContentSection = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 25px;
+
+    line-height: small;
+
+    h1 {
+        font-family: 'Nunito', sans-serif;
+        font-size: 30px;
+    }
+
+    p {
+        color: var(--grey-2);
+    }
+`
+
+export { Container, ContentSection }

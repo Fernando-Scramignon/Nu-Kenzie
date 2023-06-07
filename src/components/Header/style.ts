22a4d25cf56ca4254f97ccfcc3e81a69e32c85ac
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-07 08:33:11 -0300

    feat: creates header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.header`
+    height: 65px;
+    width: 100%;
+    padding: 0px 20px;
+
+    box-shadow: 0px 4px 32px -12px #00000040;
+    background-color: var(--grey-0);
+
+    display: flex;
+    align-items: center;
+`
+
+export { Container }

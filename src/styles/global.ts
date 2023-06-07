a7071a7f04c57ff020c0f5c43c9347bb2e1da73e
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-06 22:24:51 -0300

    style: adds color variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,17 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+:root {
+    --color-primary: #FD377E;
+    --color-primary-2: #E34981;
+    --color-secondary: #03B898;
+    
+    --grey-1: #F8F9FA;
+    --grey-2: #E9ECEF;
+    --grey-3: #868E96;
+    --grey-4: #212529;
+}
+
 * {
     margin: 0;
     padding: 0;

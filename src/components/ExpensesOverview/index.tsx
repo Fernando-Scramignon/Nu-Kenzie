86bdf2f0e0cdff4de12fc6651ed79ee63581aeaa
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-07 18:42:46 -0300

    feat: creates Empty Expensens Overview part of the component

---
+++ src/components/ExpensesOverview/index.tsx
@@ -0,0 +1,12 @@
+import { EmptyOverviewContainer } from './style'
+
+function ExpensesOverview() {
+    return (
+        <EmptyOverviewContainer>
+            <h2>Resumo financeiro</h2>
+            <p>Você ainda não possuí nenhum lançamento</p>
+        </EmptyOverviewContainer>
+    )
+}
+
+export default ExpensesOverview

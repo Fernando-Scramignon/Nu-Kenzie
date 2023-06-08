e5c51176a3eb32200dbd1958c2c9c7c96679553e
Fernando Scramignon <scramignonnarde@gmail.com>
2023-06-07 22:50:50 -0300

    build: installs react icons

--- src/components/OverviewCard/index.tsx
+++ src/components/OverviewCard/index.tsx
@@ -1,6 +1,8 @@
 import { IOverviewCardProps } from '../../interfaces'
 import { Container, TypeSection, Type, InfoSection } from './style'
 
+// import { FaTrash } from 'react-icons/fa'
+
 function OverviewCard({ type }: IOverviewCardProps) {
     return (
         <Container>
